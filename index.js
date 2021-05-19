e2f68c5e31f6b34028410652c332a5b8b3af177c
KHOA <khoavd@minori.com.vn>
2021-05-19 14:11:23 +0700

    add check milliSecond

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-let f = (delay = 1000, max) => {
-  const d = new Date();
-  let milliSecond = d.getMilliseconds();
-  if (typeof max === "number" && max > 0) {
-    for (let i = 1; i <= max; i++) {
-      setTimeout(() => {
-        console.log(`check lần ${i}`);
-        milliSecond %= 2;
-      }, delay);
-      return milliSecond !== null;
+let f = (max) => {
+  let check = () => {
+    if (!max) {
+      let milliSecond = new Date().getMilliseconds();
+      console.log(milliSecond % 2 === 0);
+      setTimeout(check, 1000);
+    } else {
+      for (let i = 1; i <= max; i++) {
+        setTimeout(() => {
+          let milliSecond = new Date().getMilliseconds();
+          console.log(milliSecond % 2 === 0);
+        }, i * 1000);
+      }
     }
-  }
-  //   console.log("123 :>> ", 123);
-  //   setTimeout(() => {
-  //     milliSecond %= 2;
-  //   }, delay);
-  //   return milliSecond === 0;
+  };
+  check();
 };
 
-console.log("f :>> ", f(3000, 3));
+f();
