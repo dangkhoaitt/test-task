e7a903f1c46ab7cd820320950d8cf6f92f387e12
KHOA <khoavd@minori.com.vn>
2021-06-08 17:29:08 +0700

    add LogNumber

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let f = (max) => {
+function f(max) {
   let check = () => {
     if (!max) {
       let milliSecond = new Date().getMilliseconds();
@@ -14,6 +14,41 @@ let f = (max) => {
     }
   };
   check();
-};
+}
 
-f();
+// function generateTime(hours) {
+//   let result = [];
+//   let i = 0;
+//   do {
+//     let a = [];
+//     i += 1;
+//     a.push("a");
+//     if (i < 5) result.push(a.join("-"));
+//   } while (i < 5);
+
+//   console.log(result);
+// }
+// generateTime(5);
+
+function delay(delayInms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(2);
+    }, delayInms);
+  });
+}
+
+async function LogNumber(arrNum) {
+  let i = 0;
+  let log = async () => {
+    await delay(1000);
+    i++;
+    console.log(arrNum[i]);
+    if (i === arrNum.length - 1) return;
+    else await log();
+  };
+  log();
+}
+
+let numbers = [0, 1, 2, 3, 4, 5];
+LogNumber(numbers);
